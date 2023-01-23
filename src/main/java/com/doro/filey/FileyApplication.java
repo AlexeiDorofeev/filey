0fc2b4b8a6e1b86d28a169f211ca0ba7133c4493
Alexei.Dorofeev <Alexei.Dorofeev@wolterskluwer.com>
2023-01-23 04:39:40 +0100

    init

---
+++ src/main/java/com/doro/filey/FileyApplication.java
@@ -0,0 +1,13 @@
+package com.doro.filey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doro/filey/FileyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doro.filey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
